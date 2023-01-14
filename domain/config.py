a40c591961255192ba79908c2a10d05e7830befd
Lukasz Klimkiewicz <ukasz.klimkiewicz@proton.me>
2023-01-14 12:09:35 +0100

    Add reversi config

--- domain/config.py
+++ domain/config.py
@@ -8,18 +8,39 @@ Game = namedtuple('Game', ['env_name', 'time_factor', 'actionSelect',
 games = {}
 
 cartpole_swingup = Game(env_name='CartPoleSwingUp',
-  actionSelect='all', # all, soft, hard
   input_size=5,
   output_size=1,
-  time_factor=0,
+  in_out_labels=['x','x_dot','cos(theta)','sin(theta)','theta_dot', 'force'],
+  actionSelect='all',
   layers=[5, 5],
   i_act=np.full(5,1),
-  h_act=[1,2,3,4,5,6,7,8,9,10],
   o_act=np.full(1,5),
+  h_act=[1,2,3,4,5,6,7,8,9,10],
+  time_factor=0,
+  weightCap = 2.0,
+  noise_bias=0.0,
+  output_noise=[False, False, False],
+  max_episode_length=1000,
+)
+games['swingup'] = cartpole_swingup
+
+
+cartpole_swingup = Game(env_name='reversi',
+  input_size=5,
+  output_size=1,
+  in_out_labels=['x','x_dot','cos(theta)','sin(theta)','theta_dot', 'force'],
+  actionSelect='all', # all, soft, hard
+  layers=[5, 5],  # anything
+
+  i_act=np.full(5,1), # (input_size, 1)
+  o_act=np.full(1,5), # (output_size, 1)
+  h_act=[1,2,3,4,5,6,7,8,9,10], # (step functions)
+
+
+  time_factor=0,
   weightCap = 2.0,
   noise_bias=0.0,
   output_noise=[False, False, False],
   max_episode_length = 1000,
-  in_out_labels = ['x','x_dot','cos(theta)','sin(theta)','theta_dot', 'force']
 )
-games['swingup'] = cartpole_swingup
+games['reversi'] = cartpole_swingup
--- domain/make_env.py
+++ domain/make_env.py
@@ -3,7 +3,9 @@ def make_env(env_name, seed=-1, render_mode=False):
     from domain.cartpole_swingup import CartPoleSwingUpEnv
     env = CartPoleSwingUpEnv()
 
-
+  elif (env_name.startswith("reversi")):
+    from domain.reversi import ReversiEnv
+    env = ReversiEnv()
 
   if (seed >= 0):
     domain.seed(seed)
